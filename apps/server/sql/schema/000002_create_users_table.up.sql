374e5308069cf9bc7da3a7f99bd592d363e255e6
Kasyap1234 <kasyap3103@gmail.com>
2025-07-23 16:02:17 +0530

    add parser

---
+++ apps/server/sql/schema/000002_create_users_table.up.sql
@@ -0,0 +1,19 @@
+
+DO $$
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'user_role') THEN
+            CREATE TYPE user_role AS ENUM ('user', 'supervisor', 'manager', 'super_admin');
+        END IF;
+    END
+$$;
+
+-- Then define the users table
+CREATE TABLE IF NOT EXISTS users (
+                                     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                                     name TEXT NOT NULL,
+                                     email TEXT UNIQUE NOT NULL,
+                                     password TEXT NOT NULL,
+                                     phone TEXT NOT NULL,
+                                     role user_role NOT NULL DEFAULT 'user',
+                                     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
