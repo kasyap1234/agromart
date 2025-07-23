1e60f08878e9c7cc2dbd8375fd274337a08eb4c6
Kasyap1234 <kasyap3103@gmail.com>
2025-07-23 10:04:06 +0530

    add auth basic

--- internal/auth/middleware.go
+++ internal/auth/middleware.go
@@ -1,1 +1,25 @@
 package auth
+
+import (
+	"agromart/pkg/errors"
+	"agromart/pkg/httpx"
+
+	"github.com/labstack/echo/v4"
+)
+
+func RequireAuth(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		authHeader := c.Request().Header.Get("Authorization")
+		if authHeader == "" {
+			return httpx.Unauthorized(c, errors.ErrUnauthorized.Error())
+		}
+		tokenStr := authHeader[len("Bearer"):]
+		claims, err := ParseToken(tokenStr)
+		if err != nil {
+			return httpx.Unauthorized(c, errors.ErrUnauthorized.Error())
+		}
+		c.Set("user_id", claims.UserID)
+		c.Set("tenant_id", claims.TenantID)
+		return next(c)
+	}
+}
