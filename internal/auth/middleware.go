766e0df77750209c9fafe056d03fcb59f53f9752
Kasyap1234 <kasyap3103@gmail.com>
2025-07-23 16:24:08 +0530

    add role check to context or add role context

--- internal/auth/middleware.go
+++ internal/auth/middleware.go
@@ -1,9 +1,11 @@
 package auth
 
 import (
+	"agromart/db"
 	"agromart/pkg/errors"
 	"agromart/pkg/httpx"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
 
@@ -18,8 +20,21 @@ func RequireAuth(next echo.HandlerFunc) echo.HandlerFunc {
 		if err != nil {
 			return httpx.Unauthorized(c, errors.ErrUnauthorized.Error())
 		}
+		uid, err := uuid.Parse(claims.UserID)
+		if err != nil {
+			return httpx.Unauthorized(c, errors.ErrUnauthorized.Error())
+		}
+		queries :=db.Queries{}
+		user,err := queries.GetUserByID(c,uid)
+		if err !=nil{
+			return httpx.Unauthorized(c errors.ErrUnauthorized.Error())
+
+		}
+
+
 		c.Set("user_id", claims.UserID)
 		c.Set("tenant_id", claims.TenantID)
+		c.Set("user_role",user.Role)
 		return next(c)
 	}
 }
