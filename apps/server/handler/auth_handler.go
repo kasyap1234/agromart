8b896defae4e7866560e86d1af9ead29d4dee590
Kasyap Dharanikota <kasyap3103@gmail.com>
2025-08-04 19:51:46 +0530

    change code exec

--- apps/server/handler/auth_handler.go
+++ apps/server/handler/auth_handler.go
@@ -1,1 +1,14 @@
 package handler
+
+import (
+	"github.com/kasyap1234/agromart/apps/server/models"
+)
+
+type RegisterRequest struct {
+	Name     string      `json:"name" validate:"required"`
+	Email    string      `json:"email" validate: "required"`
+	Password string      `json:"password" validate:"min=6"`
+	Role     models.Role `json:"role" validate:"required"`
+	Company  string      `json:"company" validate:"required"`
+	
+}
