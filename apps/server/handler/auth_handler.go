142fefc3e14dbc6d1fb31335c187baa8edc7b292
Kasyap Dharanikota <kasyap3103@gmail.com>
2025-08-04 20:13:43 +0530

    sqlc

--- apps/server/handler/auth_handler.go
+++ apps/server/handler/auth_handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/kasyap1234/agromart/apps/server/models"
 )
 
@@ -10,5 +11,16 @@ type RegisterRequest struct {
 	Password string      `json:"password" validate:"min=6"`
 	Role     models.Role `json:"role" validate:"required"`
 	Company  string      `json:"company" validate:"required"`
-	
+}
+
+type LoginRequest struct {
+	Email    string      `json:"email" validate:"required"`
+	Password string      `json:"password" validate:"min=6"`
+	Role     models.Role `json:"role" validate:"required"`
+}
+
+type CustomClaims struct {
+	ID   uint        `json:"id"`
+	Role models.Role `json:"role"`
+	jwt.RegisteredClaims
 }
